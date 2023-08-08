adc79ba5a1b27b0c2232731af52ec65b9603e289
yassinbel <yassin06belefkih@gmail.com>
2023-08-08 23:35:52 +0100

    Add files via upload

---
+++ StudentListPage.xaml.cs
@@ -0,0 +1,26 @@
+using SQLiteDemo.ViewModels;
+
+namespace SQLiteDemo.Views;
+
+public partial class StudentListPage : ContentPage
+{
+    private StudentListPageViewModel _viewMode;
+    public StudentListPage(StudentListPageViewModel viewModel)
+	{
+		InitializeComponent();
+        _viewMode = viewModel;
+        this.BindingContext = viewModel;
+	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _viewMode.GetStudentListCommand.Execute(null);
+    }
+
+    private async void OnOpenNewPageClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Intranet());
+    }
+
+}
